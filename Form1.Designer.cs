c5b41526b4d8323485c91b8d70e40e3ae7c2168b
Abdelhakiem <abdelhakim.osama150@gmail.com>
2022-05-19 02:04:02 +0200

    Task1 first version

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Function_Plotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputEquation = new System.Windows.Forms.TextBox();
+            this.MinBound = new System.Windows.Forms.TextBox();
+            this.MaxBound = new System.Windows.Forms.TextBox();
+            this.cartesianChart = new LiveCharts.WinForms.CartesianChart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(985, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Plot";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(148, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "F(x) =";
+            // 
+            // InputEquation
+            // 
+            this.InputEquation.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputEquation.Location = new System.Drawing.Point(399, 37);
+            this.InputEquation.Name = "InputEquation";
+            this.InputEquation.Size = new System.Drawing.Size(534, 34);
+            this.InputEquation.TabIndex = 4;
+            // 
+            // MinBound
+            // 
+            this.MinBound.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinBound.Location = new System.Drawing.Point(399, 83);
+            this.MinBound.Name = "MinBound";
+            this.MinBound.Size = new System.Drawing.Size(534, 32);
+            this.MinBound.TabIndex = 5;
+            // 
+            // MaxBound
+            // 
+            this.MaxBound.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxBound.Location = new System.Drawing.Point(399, 128);
+            this.MaxBound.Name = "MaxBound";
+            this.MaxBound.Size = new System.Drawing.Size(534, 33);
+            this.MaxBound.TabIndex = 6;
+            // 
+            // cartesianChart
+            // 
+            this.cartesianChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart.BackColor = System.Drawing.SystemColors.GrayText;
+            this.cartesianChart.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cartesianChart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cartesianChart.Location = new System.Drawing.Point(4, -8);
+            this.cartesianChart.Name = "cartesianChart";
+            this.cartesianChart.Padding = new System.Windows.Forms.Padding(0, 5, 5, 9);
+            this.cartesianChart.Size = new System.Drawing.Size(1213, 511);
+            this.cartesianChart.TabIndex = 7;
+            this.cartesianChart.Text = "cartesianChart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Interval Begining :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Interval End :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.groupBox1.Controls.Add(this.InputEquation);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.MinBound);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.MaxBound);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 506);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1193, 221);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inputs:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1235, 739);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cartesianChart);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Function Plotter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputEquation;
+        private System.Windows.Forms.TextBox MinBound;
+        private System.Windows.Forms.TextBox MaxBound;
+        private LiveCharts.WinForms.CartesianChart cartesianChart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using NCalc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Function_Plotter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Rules: \n1-Equation musn't be empty\n2-Min of interval must be less than max of interval" +
+                                   "\n3-Variables other than x isn't valid.\n4-Operators other than (+,-,*,/,^) is invalid." +
+                                   "\n5-Division by zero isn't allowed.");
+        }
+
+        static bool IsOperator(String ch)
+        {
+            if (ch == "^" || ch == "*" || ch == "/" || ch == "+" || ch == "-")
+        {
+                return true;
+            }
+        else
+            {
+                return false;
+            }
+        }
+
+        static   bool IsValidChar(char ch)
+        {
+            string str = ""; str += ch;
+            if (Char.IsLetter(ch) && ch != 'x')
+            {
+                MessageBox.Show("Invalid variable : " + ch);
+                return false;
+
+            }
+
+            else if (!(Char.IsLetter(ch)|| Char.IsDigit(ch)) && !IsOperator(str))
+                {
+                   MessageBox.Show("Invalid Operator : " + ch);
+                return false;
+            }
+            return true;
+        }
+
+        static bool IsMissingOperator(string prev, string curr)
+        {
+            if (prev == "x" && curr == "x")
+            {
+                return true;
+            }
+            else if ((int.TryParse(prev, out int value) && curr == "x")||(prev == "x" && (int.TryParse(curr, out int val))))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool ExctractArr(String line, ref ArrayList array)
+        {
+
+            for (int i = 0; i < line.Length; i++){
+                string curr = ""; curr += line[i];
+
+                if (curr == " ") {
+                    continue;
+                }
+                else if ((array.Count != 0))
+                {
+                   if(!IsValidChar(line[i])){
+                        return false;
+                    }
+
+                    string prev = (string)array[array.Count - 1];
+                    if (int.TryParse(prev, out int value) && int.TryParse(curr, out int Val)){
+                        array.RemoveAt(array.Count - 1);
+                        array.Add(prev + curr);
+                        continue;
+                    }
+                    else if (prev == "/" && curr == "0"){
+                        MessageBox.Show("Division by zero not allowed");
+                        return false;
+                    }
+                    else if (IsOperator(prev) && IsOperator(curr)){
+                        MessageBox.Show("Missing variable or number between "+prev+" and "+curr);
+                        return false;
+                    }
+                    else if (IsMissingOperator(prev, curr)) {
+                        MessageBox.Show("Missing operator between " + prev + " and " + curr);
+                        return false;
+                    }
+                    else {
+                        array.Add(curr);
+                    }
+                }
+                else
+                {
+                    if (!IsValidChar(line[i]))
+                    {
+                        return false ;
+                    }
+                    array.Add(curr);
+                }
+
+
+
+            }
+            return true;
+        }
+
+       public static  double Function(string equation, double value)
+        {
+            var expr = new NCalc.Expression(equation);
+            expr.Parameters["x"] = value;
+            return ((double)(expr.Evaluate()));
+        }
+        static String GetEquation(ArrayList array)
+        {
+            StringBuilder equation = new StringBuilder(); String str;
+            for (int i = 0; i < array.Count; i++)
+            {
+                str= (string)array[i];
+                if (str == "^")
+                {
+                    i++;
+                    int cnt = Convert.ToInt32(array[i]);
+                    for (int j = 0; j < cnt - 1; j++)
+                    {
+                        equation.Append("* x ");
+                    }
+                }
+                else
+                {
+                    equation.Append(str + " ");
+                }
+            }
+            return equation.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double MinValue = Convert.ToDouble(MinBound.Text);
+            double MaxValue = Convert.ToDouble(MaxBound.Text);
+            if (MinValue>=MaxValue)
+            {
+                MessageBox.Show("Value of minimum of interval should be less than Maximum of the interval.");
+                return;
+            }
+            else if (InputEquation.Text == "")
+            {
+                MessageBox.Show("Equation musn't be empty.");
+            }
+            ArrayList array = new ArrayList();
+            if (!ExctractArr(InputEquation.Text, ref array)) {
+                return;
+            };
+            String equation = GetEquation(array);
+
+            ChartValues<ObservablePoint> List1Points = new ChartValues<ObservablePoint>();
+            for (double i = MinValue; i <= MaxValue; i+=0.2)
+            {
+
+                List1Points.Add(new ObservablePoint
+                {
+                    X = i,
+                    Y = Function(equation, i)
+                });
+            }
+            cartesianChart.Series = new SeriesCollection {
+               new LineSeries{
+                Values=List1Points,PointGeometrySize=5
+
+               } 
+            };
+
+
+        }
+
+
+
+    }
+}
